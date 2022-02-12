00e68d844351929fc13fa728b9c7418d49f37aa8
kiaruh <nikoz.li@gmail.com>
2022-02-12 17:35:08 -0300

    create an stack

--- stacks.js
+++ stacks.js
@@ -19,10 +19,68 @@ for (var i = 0; i < word.length; i++) {
     reverseWord += letters.pop()
 }
 
+// compare reverseWord with word
 if(reverseWord === word) {
     console.log('The word is a palindrome')
 }else{
     console.log('The word is not a palindrome')
 }
 
-console.log(reverseWord)
+// console.log(reverseWord)
+
+// MAKING A STACK on my own
+
+var stack = function(){
+    this.count = 0
+    this.storage = {}
+
+    //Adds a value onto the end of the stack
+    this.push = (value) => {
+        this.storage[this.count] = value
+        this.count++
+    }
+
+    //Removes and returns the value at the end of the stack
+    this.pop = () => {
+        if(this.count === 0){
+            return undefined
+        }
+        //if there is a value in the stack then we will remove it and return it
+        this.count--
+        var result = this.storage[this.count]
+        delete this.storage[this.count]
+        return result
+    }
+
+    //Size returns the number of items in the stack
+    this.size = () => {
+        return this.count
+    }
+
+    //Returns the value at the end of the stack
+    this.peek = () => {
+        return this.storage[this.count - 1]
+    }
+    //Returns true if the stack is empty, and false otherwise
+    this.isEmpty = () => {
+        return this.count === 0
+    }
+    //Returns the whole stack
+    this.print = () => {
+        return this.storage
+    }
+}
+
+var myStack = new stack()
+
+myStack.push('a')
+myStack.push('b')
+myStack.push(3)
+console.log(myStack.print())
+console.log(myStack.size())
+console.log(myStack.peek())
+console.log(myStack.pop())
+console.log(myStack.print())
+console.log(myStack.size())
+
+
