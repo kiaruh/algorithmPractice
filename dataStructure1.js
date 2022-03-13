66d5e515d8ce090692386ee2e8f9901cfb73066e
kiaruh <nikoz.li@gmail.com>
2022-03-13 02:03:06 -0300

    otra forma de resovler una palabra palindroma

--- dataStructure1.js
+++ dataStructure1.js
@@ -1,2 +1,9 @@
 //Free code camp Data Structure 1
 
+//otra forma de resolver un palindromo
+let palabra = 'onomatopeya'
+let palabraReverse = ''
+for(let i = 1; i <= palabra.length; i++){
+   palabraReverse += palabra[palabra.length - i]
+}
+console.log(palabraReverse);
