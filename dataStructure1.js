10ded1e3ddeee69b12146d39b52868450ddd3383
kiaruh <nikoz.li@gmail.com>
2022-03-13 03:31:37 -0300

    mas funciones de arreglos

--- dataStructure1.js
+++ dataStructure1.js
@@ -10,8 +10,18 @@ console.log(palabraReverse);
 
 //Arreglo multidiimensional
 let arreglo = [[1,2,3],[4,5,6],[7,8,9],10,11,12]
-console.log(arreglo[0][1])
-console.log(arreglo[1][2])
-console.log(arreglo[2][0])
-console.log(arreglo[3])
+// console.log(arreglo[0][1]) //2
+// console.log(arreglo[1][2]) //8
+// console.log(arreglo[2][0]) //7
+// console.log(arreglo[3]) //10
+
+//funciones de arreglos - push, pop, shift (elimina el primero), unshift (agrega al principio)
+let arr1 = ['cat', 'dog', 'rabbit']
+let arr2 = ['red', 'blue', 'green']
+let arr3 = ['a', 'b', 'c']
+arr2.push(arr1)
+console.log(arr2)
+arr2.shift() // elimina el primer elemento
+arr2.unshift(arr3) // agrega al principio
+console.log(arr2)
 
