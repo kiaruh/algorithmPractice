fbb249ba4f22c55640d931392c4fb37d163ba2d6
kiaruh <nikoz.li@gmail.com>
2022-03-13 03:16:05 -0300

    arreglos multidimensionales

--- dataStructure1.js
+++ dataStructure1.js
@@ -7,3 +7,11 @@ for(let i = 1; i <= palabra.length; i++){
    palabraReverse += palabra[palabra.length - i]
 }
 console.log(palabraReverse);
+
+//Arreglo multidiimensional
+let arreglo = [[1,2,3],[4,5,6],[7,8,9],10,11,12]
+console.log(arreglo[0][1])
+console.log(arreglo[1][2])
+console.log(arreglo[2][0])
+console.log(arreglo[3])
+
