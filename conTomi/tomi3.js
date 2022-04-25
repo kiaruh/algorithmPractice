086d415ef4bf1d99634b11a5a9ced79b42922b49
kiaruh <nikoz.li@gmail.com>
2022-04-25 16:07:57 -0300

    mas cosas con tomi

---
+++ conTomi/tomi3.js
@@ -0,0 +1,23 @@
+let colores = [
+    "Amarillo",
+    "Rojo",
+    "Azul",
+    "Verde",
+    "Naranja",
+    "Violeta",
+    "Marrón",
+  ];
+
+
+// for (let i = 0; i < colores.length; i++) {
+//   if (
+//     i > colores[3]; false
+//     )
+//     console.log(colores[i]);
+//   } //ORIGINAL
+
+for(let i = 0; i< colores.length; i++){
+   if(i != 3){ //i distinto a 3
+         console.log(colores[i]);
+   }
+} //posible respuesta
