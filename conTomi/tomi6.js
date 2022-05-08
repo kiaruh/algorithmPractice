ebf40317082da909237f57e5727fe46d6388938c
kiaruh <nikoz.li@gmail.com>
2022-05-08 20:23:51 -0300

    mas ejemplos

---
+++ conTomi/tomi6.js
@@ -0,0 +1,19 @@
+// Esto es una variable, puede ser let o var
+let numero = 1;
+var nombre = 'pedro';
+let otraVariable = true;
+
+let arregloDeNombres = ['pedro', 'juan', 'maria']; //0,1,2
+
+//create a loop that will print the numbers from 1 to 10
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// console.log(arregloDeNombres);
+console.log(arregloDeNombres[0]);
+console.log(arregloDeNombres[1]);
+console.log(arregloDeNombres[2]);
+// console.log(arregloDeNombres.length);
+
+for (let i = 0; i < arregloDeNombres.length; i++) {console.log(arregloDeNombres[i]);}
