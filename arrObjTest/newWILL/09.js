4c8dedc567a8eed086a1a973e62f260e5e6caaae
kiaruh <nikoz.li@gmail.com>
2022-10-19 23:10:45 -0300

    09

--- arrObjTest/newWILL/09.js
+++ arrObjTest/newWILL/09.js
@@ -4,29 +4,54 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function filtrar(funcion) {
-  // Escribi una función filtrar en el prototipo de Arrays,
-  // que recibe una funcion (callback) que devuelve true o false.
-  // filtrar los elementos de ese arreglo en base al resultado de esa funcion
-  // comparadora, devolver un nuevo arreglo con los elementos filtrados.
-  // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
-  // ej:
-  // var productos = [{
-  //   price: 100,
-  //   name: 'tv'
-  // }, {
-  //   price: 50,
-  //   name: 'phone'
-  // }, {
-  //   price: 30,
-  //   name: 'lamp'
-  // }]
-  // productos.filtrar(function(p) {
-  //   return p.price >= 50;
-  // }) => [{price: 100, name:'tv'}]
+// function filtrar(funcion) {
+//   // Escribi una función filtrar en el prototipo de Arrays,
+//   // que recibe una funcion (callback) que devuelve true o false.
+//   // filtrar los elementos de ese arreglo en base al resultado de esa funcion
+//   // comparadora, devolver un nuevo arreglo con los elementos filtrados.
+//   // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
+//   // ej:
+  
+//   // productos.filtrar(function(p) {
+//   //   return p.price >= 50;
+//   // }) => [{price: 100, name:'tv'}]
 
-};
+
+// };
 
 // No modifiques nada debajo de esta linea //
 
-module.exports = filtrar
+Array.prototype.filtrar = function (cb){
+let arr = []
+this.forEach( i => {
+  if(cb(i)){
+    arr.push(i)
+}})
+  return arr
+}
+
+let productos = [{
+  price: 100,
+  name: 'tv'
+}, {
+  price: 50,
+  name: 'phone'
+}, {
+  price: 30,
+  name: 'lamp'
+}, {
+  price: 51,
+  name: 'algo1'
+}, {
+  price: 10,
+  name: 'algo2'
+}, {
+  price: 52,
+  name: 'algo3'
+}]
+
+let filtrado = productos.filtrar(function(item){
+  return item.price > 50
+})
+
+console.log(filtrado)
